19d95d438a2c3e2699d84ebd8de5c5621bf28f7b
Zo Sarillana <zhou991818@gmail.com>
2024-06-16 07:10:36 +0800

    added logo and new name of page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { useState } from "react";
-import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 import "./App.css";
 import bottomImg from "./assets/parallax/bottom-min.png";
 import middleImg from "./assets/parallax/middle-min.png";
 import topImg from "./assets/parallax/top-min.png";
 import overallImg from "./assets/parallax/overall.png";
-import circleLogo from "./assets/parallax/logo-min.png";
 import boxLogo from "./assets/parallax/box-logo-min.png";
 import Header from "./views/partials/Header";
 import ParallaxComponent from "./views/parallax/Parallax";
